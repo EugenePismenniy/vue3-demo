b1e5217dc22da80aa5e2937d442dbb338c0dbd74
evhen <evgeniy.pismenny@gmail.com>
2022-03-18 23:23:19 +0200

    1

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,85 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+<div class="app">
+  <h1>Страница с постами</h1>
+  <my-button style="margin: 15px 0;" @click="showDialog">Создаит пост</my-button>
+  <my-dialog v-model:show="dialogVisible">
+    <post-form @create="createPost"/>
+  </my-dialog>
+  <post-list :posts="posts" @remove="removePost"/>
+
+</div>
 </template>
 
+<script>
+
+import PostForm from "@/components/PostForm";
+import PostList from "@/components/PostList";
+import axios from 'axios';
+
+export default {
+
+  components: {
+    PostList, PostForm
+  },
+
+  data() {
+    return {
+      posts: [
+        {id: 1, title: 'Какой то пост 1', body: 'Какой то пост о чем то там 1'},
+        {id: 2, title: 'Какой то пост 2', body: 'Какой то пост о чем то там 2'},
+        {id: 3, title: 'Какой то пост 3', body: 'Какой то пост о чем то там 3'},
+        {id: 4, title: 'Какой то пост 4', body: 'Какой то пост о чем то там 4'},
+      ],
+      dialogVisible: false
+    }
+  },
+  methods: {
+
+    createPost(post) {
+
+      this.posts.push(post);
+      this.dialogVisible = false;
+    },
+    removePost(post) {
+
+      this.posts = this.posts.filter(p => p.id !== post.id);
+    },
+    showDialog() {
+      this.dialogVisible = true;
+    },
+
+    async fetchPosts() {
+      try {
+
+        let response = await axios.get('https://jsonplaceholder.typicode.com/posts?_limit=10', {
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        });
+
+        console.log(response);
+
+      } catch (e) {
+        console.log(e);
+        alert('Error');
+      }
+    }
+
+  }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 
-nav {
-  padding: 30px;
+.app {
+  padding: 20px;
 }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
 
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
---
+++ src/components/PostForm.vue
@@ -0,0 +1,43 @@
+<template>
+  <form @submit.prevent>
+    <h4>Создание поста</h4>
+    <my-input v-model.trim="post.title" placeholder="Название"/>
+    <my-input v-model.trim="post.body" placeholder="Описание"/>
+    <my-button style="align-self: flex-end; margin-top: 15px;" @click="createPost">Создвать</my-button>
+  </form>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      post: {
+        title: '',
+        body: ''
+      }
+    }
+  },
+  methods: {
+    createPost() {
+
+      this.post.id = Date.now();
+      this.$emit('create', this.post);
+      this.post = {
+        title: '',
+        body: ''
+      };
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+
+form {
+  display: flex;
+  flex-direction: column;
+}
+</style>
